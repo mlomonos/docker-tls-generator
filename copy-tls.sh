0dbd8ebdfadbecd3b507bf6d5c9bb8bbdba70517
Maksym Lomonos <mlomonos@devforth.io>
2023-11-02 13:36:10 +0200

    added script to activate tls on existing certificates [4]

--- copy-tls.sh
+++ copy-tls.sh
@@ -4,5 +4,8 @@ IP_FROM=$1
 IP_TO=$2
 USER=$3 || "root"
 
-scp $USER@$IP_FROM:~/.docker/* $USER@$IP_TO:~/.docker/
-scp $USER@$IP_FROM:/etc/docker/ssl/* $USER@$IP_TO:/etc/docker/ssl/
+echo "USER: $USER"
+echo "IPS: $IP_FROM > $IP_TO"
+
+scp -3 $USER@$IP_FROM:~/.docker/* $USER@$IP_TO:~/.docker/
+scp -3 $USER@$IP_FROM:/etc/docker/ssl/* $USER@$IP_TO:/etc/docker/ssl/
